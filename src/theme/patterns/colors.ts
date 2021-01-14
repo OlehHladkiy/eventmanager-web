1ce48e062144a639ed800ca956103129f93b5be8
Oleg <oleg.h@develops.today>
2021-01-14 13:01:17 +0200

    feat(store): implement basic style theme

---
+++ src/theme/patterns/colors.ts
@@ -0,0 +1,31 @@
+import * as R from 'ramda';
+
+export const colorScheme = {
+  white: { value: '#FFF', name: 'White' },
+  black: { value: '#000', name: 'Black' },
+  blue: { value: '#1890ff', name: 'Blue' },
+  boldBlue: { value: '#1a6fe0', name: 'boldBlue' },
+  lightBlackTone: { value: '#35475c', name: 'lightBlackTone' },
+  gray: { value: '#e8e8e8', name: 'Gray' },
+  red: { value: '#ff0000', name: 'Red' },
+  green: { value: '#008000', name: 'Green' },
+};
+
+export const getColor = (colorKey: string) =>
+  R.path([colorKey, 'value'], colorScheme);
+
+const getColorsMap = () =>
+  R.compose(
+    R.reduce(
+      (
+        acc: Record<string, any>,
+        [key, scheme]: [string, Record<string, any>],
+      ) => R.assoc(key, scheme.value, acc),
+      {},
+    ),
+    R.toPairs,
+  )(colorScheme);
+
+const colors = getColorsMap();
+
+export default colors;
---
+++ src/theme/patterns/shadows.ts
@@ -0,0 +1,7 @@
+const shadows = {
+  softOuterShadow: '0 5px 10px 0 rgba(9,8,61,0.04)',
+  altOuterShadow: 'rgba(71, 88, 114, 0.14) 0 2px 2px',
+  altOuterShadowHover: 'rgba(71, 88, 114, 0.16) 0 4px 16px',
+};
+
+export default shadows;
