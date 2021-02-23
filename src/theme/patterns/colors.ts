77d407f3907ec14bc97ca295ee625dfe9f129048
Oleg <oleg.h@develops.today>
2021-02-23 14:39:35 +0200

    chore(theme): update colors

--- src/theme/patterns/colors.ts
+++ src/theme/patterns/colors.ts
@@ -3,8 +3,12 @@ import * as R from 'ramda';
 export const colorScheme = {
   white: { value: '#FFF', name: 'White' },
   black: { value: '#000', name: 'Black' },
+  almostBlack: { value: '#1e0a3c', name: 'AlmostBlack' },
+  lightGray: { value: '#39364f', name: 'LightGray' },
   blue: { value: '#1890ff', name: 'Blue' },
+  grayishBlack: { value: '#DBDAE3', name: 'GrayishBlack' },
   boldBlue: { value: '#1a6fe0', name: 'boldBlue' },
+  boldGray: { value: '#EEEDF2', name: 'boldGray' },
   lightBlackTone: { value: '#35475c', name: 'lightBlackTone' },
   gray: { value: '#e8e8e8', name: 'Gray' },
   red: { value: '#ff0000', name: 'Red' },
