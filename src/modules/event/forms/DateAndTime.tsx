9c85dedff2a453952ce76340f4c69b103bda00a0
Oleg <oleg.h@develops.today>
2021-02-24 00:48:50 +0200

    chore(event): update date and time form

--- src/modules/event/forms/DateAndTime.tsx
+++ src/modules/event/forms/DateAndTime.tsx
@@ -6,28 +6,54 @@ import FormWrapper from '@components/FormWrapper';
 
 import { EventScheduleType } from '../models';
 
-const DateAndTime = () => (
-  <FormWrapper
-    title="Date and time"
-    description="ell event-goers when your event starts and ends so they can make plans to attend."
-    icon={<CalendarOutlined />}
-  >
-    <Form.Item name="schedule_type">
-      <Radio.Group>
-        <Radio.Button value={EventScheduleType.Single}>
-          Single Event
-        </Radio.Button>
-        <Radio.Button value={EventScheduleType.Recurring}>
-          Recurring Events
-        </Radio.Button>
-      </Radio.Group>
-    </Form.Item>
-    <Form.Item name="date">
-      <DatePicker.RangePicker
-        showTime={{ showHour: true, showMinute: true, showSecond: false }}
-      />
-    </Form.Item>
-  </FormWrapper>
-);
+const DateAndTime = () => {
+  const getFormItemByScheduleType = (scheduleType: EventScheduleType) => {
+    switch (scheduleType) {
+      case EventScheduleType.Single: {
+        return (
+          <Form.Item name="date">
+            <DatePicker.RangePicker
+              showTime={{ showHour: true, showMinute: true, showSecond: false }}
+            />
+          </Form.Item>
+        );
+      }
+      default: {
+        return (
+          <span>
+            If your event repeats or occurs more than once, you can schedule
+            additional times and dates in the Schedule tab. Event details and
+            ticket types will automatically appear on all event occurrences and
+            can&apos;t be customized for an individual occurrence.
+          </span>
+        );
+      }
+    }
+  };
+
+  return (
+    <FormWrapper
+      title="Date and time"
+      description="ell event-goers when your event starts and ends so they can make plans to attend."
+      icon={<CalendarOutlined />}
+    >
+      <Form.Item name="schedule_type">
+        <Radio.Group>
+          <Radio.Button value={EventScheduleType.Single}>
+            Single Event
+          </Radio.Button>
+          <Radio.Button value={EventScheduleType.Recurring}>
+            Recurring Events
+          </Radio.Button>
+        </Radio.Group>
+      </Form.Item>
+      <Form.Item dependencies={['schedule_type']}>
+        {({ getFieldValue }) =>
+          getFormItemByScheduleType(getFieldValue('schedule_type'))
+        }
+      </Form.Item>
+    </FormWrapper>
+  );
+};
 
 export default DateAndTime;
