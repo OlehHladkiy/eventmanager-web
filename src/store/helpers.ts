0de1b830d0e6cbb1b0e055d4e3b6b50bc20e4c3a
Oleg <oleg.h@develops.today>
2021-03-09 23:56:47 +0200

    chore(store): fix getReduxAction helper

--- src/store/helpers.ts
+++ src/store/helpers.ts
@@ -3,4 +3,4 @@ import { ActionSuffix } from './models';
 export const getReduxAction = (type: string, payload?: Record<string, any>) =>
   payload
     ? { type: `${type}${ActionSuffix.Success}`, payload }
-    : { type: `${type}${ActionSuffix.Success}` };
+    : { type: `${type}${ActionSuffix.Fail}` };
